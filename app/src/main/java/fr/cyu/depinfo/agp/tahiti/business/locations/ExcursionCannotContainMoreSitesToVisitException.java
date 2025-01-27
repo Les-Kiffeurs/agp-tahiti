47cb48d38134a5e3bd991c8f8aab96fb90cfefce
Thomas REMY <contact@thomasremy.fr>
2025-01-28 00:40:04 +0100

    feat(transport): Add tranport and offer models
    Introduced several new classes to handle the business logic. This includes models for transport, excursions, and offers. Also added an exception class to handle cases when an excursion cannot contain more sites to visit. Updated the Spring beans configuration file to include these new classes.

---
+++ app/src/main/java/fr/cyu/depinfo/agp/tahiti/business/locations/ExcursionCannotContainMoreSitesToVisitException.java
@@ -0,0 +1,7 @@
+package fr.cyu.depinfo.agp.tahiti.business.locations;
+
+public class ExcursionCannotContainMoreSitesToVisitException extends Exception {
+    public ExcursionCannotContainMoreSitesToVisitException(String message) {
+        super(message);
+    }
+}
