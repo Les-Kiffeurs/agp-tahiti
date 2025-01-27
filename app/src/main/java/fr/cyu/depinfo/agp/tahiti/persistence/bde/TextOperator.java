a34f6c864c6e8670094956af071f9a893dca5471
Barberey <riedel.romain@gmail.com>
2025-01-27 22:29:43 +0100

    Adding Lucene logic

--- app/src/main/java/fr/cyu/depinfo/agp/tahiti/persistence/bde/TextOperator.java
+++ app/src/main/java/fr/cyu/depinfo/agp/tahiti/persistence/bde/TextOperator.java
@@ -3,6 +3,9 @@ package fr.cyu.depinfo.agp.tahiti.persistence.bde;
 import java.util.Map;
 
 public class TextOperator extends AbstractFinalOperator{
+
+    private TextResults textResults;
+
     @Override
     public void init(String query) {
 
