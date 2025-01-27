76d4fcb287e76c4825fe81fac21e98c8b0adb3c6
Barberey <riedel.romain@gmail.com>
2025-01-27 19:05:36 +0100

    Adding Execution plan classes

---
+++ app/src/main/java/fr/cyu/depinfo/agp/tahiti/persistence/bde/TextOperator.java
@@ -0,0 +1,15 @@
+package fr.cyu.depinfo.agp.tahiti.persistence.bde;
+
+import java.util.Map;
+
+public class TextOperator extends AbstractFinalOperator{
+    @Override
+    public void init(String query) {
+
+    }
+
+    @Override
+    public Map<String, String> next() {
+        return Map.of();
+    }
+}
