1bcb53ecb7abd457f03c8d98281a75b16c9f0f11
Barberey <riedel.romain@gmail.com>
2025-01-30 23:23:37 +0100

    Adding simple exception

---
+++ app/src/main/java/fr/cyu/depinfo/agp/tahiti/persistence/bde/lucene/LuceneParameterNotSet.java
@@ -0,0 +1,7 @@
+package fr.cyu.depinfo.agp.tahiti.persistence.bde.lucene;
+
+public class LuceneParameterNotSet extends RuntimeException {
+    public LuceneParameterNotSet(String message) {
+        super(message);
+    }
+}
